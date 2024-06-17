a9afde04149c03ad625b5dfabbc9967550bf8b2c
Tam <enquiretam@gmail.com>
2024-06-17 15:10:06 +0100

    Added JUnit and created a simple test

---
+++ JavaBasics/src/test/java/com/sparta/tp/ProgramTest.java
@@ -0,0 +1,12 @@
+package com.sparta.tp;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ProgramTest {
+
+    @Test
+    public void basicTest() {
+        Assertions.assertTrue(5 == 5);
+    }
+}
