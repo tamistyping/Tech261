55842e5279407608d41273c05ad861945bd7c70c
Tam <enquiretam@gmail.com>
2024-06-20 09:03:54 +0100

    whitespace removal

--- CodingKatas/src/test/java/ConsecutiveIntsTest.java
+++ CodingKatas/src/test/java/ConsecutiveIntsTest.java
@@ -2,11 +2,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
 public class ConsecutiveIntsTest {
 
     @Test
