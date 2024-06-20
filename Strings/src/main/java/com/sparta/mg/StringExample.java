c7c8cc66d3c38e189bff49b3e56e907f32c7298e
Tam <enquiretam@gmail.com>
2024-06-20 11:19:33 +0100

    string examples

---
+++ Strings/src/main/java/com/sparta/mg/StringExample.java
@@ -0,0 +1,13 @@
+package com.sparta.mg;
+
+public class StringExample {
+    public static void main(String[] args) {
+        String string1 = "hello";
+        String uppercase = string1.toUpperCase(); //immutable
+        System.out.println(uppercase);
+
+        System.out.println(string1.toCharArray());
+
+
+    }
+}
