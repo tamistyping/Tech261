fdaed348c72676f2d30ee98848cf906bdbddf0a6
Tam <enquiretam@gmail.com>
2024-07-16 10:02:45 +0100

    spring-rest template

---
+++ Spring/WeekFiveProject/src/main/java/com/sparta/tp/WeekFiveProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.tp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekFiveProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeekFiveProjectApplication.class, args);
+    }
+
+}
